02565ea49394ea43e2c4b854d666947907dbcb83
yuyuane <yuyuane@163.com>
2017-07-19 16:32:27 +0800

    edit

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\index\Controller;
 use think\Controller;
-use think\Paginator;
 use think\Db;
 class Index extends Controller{
     public function index(){
@@ -9,7 +8,7 @@ class Index extends Controller{
         $list = Db::name('message')->where(['group_id'=>$group_id])->order('id desc')->limit(5)->select();
         sort($list);
         //echo '<pre>';
-       // print_r($list);exit;
+         //print_r($list);exit;
         $this->assign('data',$list);
         return $this->fetch('weixin');
     }
